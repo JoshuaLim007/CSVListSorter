813120a475aba20493cf62ed0be53d111695b0ef
Joshua Lim <66184501+JoshuaLim007@users.noreply.github.com>
2021-01-12 16:33:47 -0800

    GitPages

---
+++ Scripts.js
@@ -0,0 +1,165 @@
+// JavaScript source code
+
+
+
+function SetCSVData(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+
+    var text = reader.readAsText(file);
+
+    reader.onload = function (event) {
+        let CSVDATA = reader.result;
+        ParseData(CSVDATA);
+    }
+}
+
+let DataArray = new Array();
+let KeyArrays = new Array();
+
+function ParseData(csvData) {
+    var lines = csvData.split('\n');
+    
+    var result = lines.map(function (line) {
+        
+        return line.split(',');
+    });
+
+    DataArray = result;
+
+    for (var i = 0; i < DataArray[0].length; i++) {
+        KeyArrays[i] = DataArray[0][i];
+    }
+    DisplayKeys();
+    DisplayCSV(DataArray);
+
+}
+
+function DisplayKeys() {
+    const par = document.getElementById("SortByContainer");
+    par.innerHTML = "";
+
+    for (var i = 0; i < KeyArrays.length; i++) {
+        const button = document.createElement('button');
+
+        button.innerHTML = "Sort By: " + KeyArrays[i];
+
+        button.id = i;
+
+        button.onclick = function () {
+
+            SortData(button.id);
+
+        };
+
+        par.appendChild(button);
+    }
+}
+
+function DisplayCSV(data) {
+
+    const parent = document.getElementById("csvDataContainer");
+
+    while (parent.firstChild) {
+        parent.removeChild(parent.lastChild);
+    }
+
+    var table = document.createElement('table');
+    var tBody = document.createElement('tbody');
+    for (var y = 0; y < data.length; y++) {
+        var row = document.createElement('tr');
+        for (var x = 0; x < data[y].length; x++) {
+            var cell = document.createElement('td');
+
+            cell.appendChild(document.createTextNode(data[y][x]));
+
+            row.appendChild(cell);
+        }
+        tBody.appendChild(row);
+    }
+    table.appendChild(tBody);
+
+    parent.appendChild(table);
+
+}
+
+function SortData(key) {
+    var toString = Object.prototype.toString;
+    let isString = toString.call(DataArray[1][key]) == '[object String]';
+
+    let length = DataArray.length - 1;
+    while (length > 0) {
+
+        for (var y = 1; y < length - 1; y++) {
+
+            let left = DataArray[y][key];
+            let right = DataArray[y + 1][key];
+
+            if (left > right) {
+
+
+                for (var i = 0; i < DataArray[y].length; i++) {
+
+
+                    var temp = DataArray[y + 1][i];
+
+
+                    DataArray[y + 1][i] = DataArray[y][i];
+
+
+                    DataArray[y][i] = temp;
+
+
+                }
+            }
+
+        }
+
+        length--;
+    }
+
+    DisplayCSV(DataArray);
+
+    ShowDownloadButton();
+}
+
+function ShowDownloadButton() {
+
+    const par = document.getElementById("finalizeContainer");
+    if (par.childElementCount == 0) {
+        var button = document.createElement("button");
+
+        button.onclick = function () {
+
+            DownloadAsCSV();
+
+        };
+
+        button.id = "DownloadBut";
+
+        button.innerHTML = "Save";
+
+        par.append(button);
+    }
+}
+
+function DownloadAsCSV() {
+
+
+    var lineArray = [];
+    DataArray.forEach(function (infoArray, index) {
+        var line = infoArray.join(",");
+        lineArray.push(index == 0 ? "data:text/csv;charset=utf-8," + line : line);
+    });
+    var csvContent = lineArray.join("\n");
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link);
+
+    link.click();
+
+
+}
